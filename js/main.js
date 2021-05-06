195aaa7bfda728439ffdeadd66d73d960bf9e196
Viktoria <73180328+Victoria0209@users.noreply.github.com>
2021-05-06 16:57:50 +0300

    14-1-carousel connection

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(".owl-carousel").owlCarousel({
-  loop:true,
-  nav:true,
-  dots:false,
+  loop: true,
+  nav: true,
+  dots: false,
+  center: true,
+  responsive: {
+    0: {
+      items: 1
+    },
+    769: {
+      items: 3
+    },
+    1124: {
+      items: 3
+      
+    }
+  }
 });
